f604879d9ea71a813b39b945802b8a5ed6da71aa
Matheus Alves <matheusalves.bmd@gmail.com>
2022-02-16 01:04:08 -0300

    Add js for the simple color flipper
    Let the user click the button to change the background color to a radom
    color from an specific set

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,1 +1,20 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+// DOM manipulation
+const btn = document.getElementById("btn");
+const colorText = document.querySelector(".color");
+
+// Event handling
+btn.addEventListener("click", function () {
+  // Generate random number from 0 to 3
+  let idx = getRandomInt(colors.length);
+  // Set the color of the body to be the drawn color
+  document.body.style.backgroundColor = colors[idx];
+  // Set background color text equal to the drawn color text
+  colorText.textContent = colors[idx];
+});
+
+// Function definition
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}

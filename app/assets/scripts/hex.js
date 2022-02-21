221bcfe7428642b890a7de79be091b81df9c8980
Matheus Alves <matheusalves.bmd@gmail.com>
2022-02-21 14:10:27 -0300

    Generate a radom background color
    Create a function to generate one of the possible HEX colors when the
    user clicks the button.

--- app/assets/scripts/hex.js
+++ app/assets/scripts/hex.js
@@ -1,1 +1,21 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+// DOM manipulation
+const btn = document.getElementById("btn");
+const colorText = document.querySelector(".color");
+
+// Event handling
+btn.addEventListener("click", () => {
+  let color = generateRandomColor();
+  colorText.textContent = color;
+  document.body.style.backgroundColor = color;
+});
+
+// Function definiton
+function generateRandomColor() {
+  let colorHEXCode = "#";
+  for (i = 0; i < 6; i++) {
+    colorHEXCode += hex[Math.floor(Math.random() * hex.length)];
+  }
+  return colorHEXCode;
+}
